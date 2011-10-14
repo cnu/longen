82c73096003dccdc0e243752e848fb0fbfd191e2
Srinivasan R <srinivasanr@gmail.com>
2011-10-15 00:31:35 +0530

    Refactored the entire code using gen.engine decorator instead of callback.
    Changed both handlers to use yield instead of callbacks. Easier to follow now.

--- main.py
+++ main.py
@@ -4,56 +4,56 @@ import traceback
 import tornado.ioloop
 import tornado.web
 from tornado.curl_httpclient import CurlAsyncHTTPClient
+from tornado import gen
+
+import brukva
 
 settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
         )
 
-def get_head(url, callback):
-    http_client = CurlAsyncHTTPClient()
-    http_client.fetch(url.strip().encode('ascii'), callback, method='HEAD')
+c = brukva.Client()
+c.connect()
+http_client = CurlAsyncHTTPClient()
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("main.html")
 
 class ExpandHandler(tornado.web.RequestHandler):
-    def handle_request(self, response):
-        try:
-            if response.error:
-                print "Error:", response.error
-            else:
-                print self.short_url
-                actual_url = response.effective_url
-                self.render("expanded.html", short_url=self.short_url, actual_url=actual_url)
-        except:
-            print traceback.format_exc()
-    
     @tornado.web.asynchronous
+    @gen.engine
     def get(self):
-        print self.request.arguments
-        self.short_url = self.request.arguments['url'][0] # TODO: Maybe take in multiple URLs?
-        get_head(self.short_url, self.handle_request)
+        short_url = self.request.arguments['url'][0].strip().encode('ascii') # TODO: Maybe take in multiple URLs?
+        # result = yield gen.Task(c.hgetall, self.short_url)
+        # print result
+
+        # c.hgetall(self.short_url, redis_callback)
+        # get_head(self.short_url, self.handle_request)
+        response = yield gen.Task(http_client.fetch, short_url, method='HEAD')
+        if response.error:
+            print response.error
+            print "Error for %s: %s" %(short_url, str(response.error))
+            actual_url = 'Error reading from URL'
+        else:
+            actual_url = response.effective_url
+        self.render("expanded.html", short_url=short_url, actual_url=actual_url)
 
 
 class ExpandRedirectHandler(tornado.web.RequestHandler):
-    def handle_redirect_request(self, response):
-        try:
-            if response.error:
-                print "Error:", response.error
-            else:
-                self.redirect(response.effective_url, permanent=True)
-        except:
-            print traceback.format_exc()
-
     @tornado.web.asynchronous
+    @gen.engine
     def get(self):
-        try:
-            short_url = self.request.arguments['url'][0]
-            get_head(short_url, self.handle_redirect_request)
-        except:
-            print traceback.format_exc()
+        short_url = self.request.arguments['url'][0].strip().encode('ascii')
+        response = yield gen.Task(http_client.fetch, short_url, method='HEAD')
+        if response.error:
+            print response.error
+            print "Error for %s: %s" %(short_url, str(response.error))
+            self.redirect('/')
+        else:
+            self.redirect(response.effective_url, permanent=True)
+
 
 application = tornado.web.Application([
     (r"/", MainHandler),
