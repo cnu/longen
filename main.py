9da363eafff9466ff8784fef5c69de8d5afd1cdf
Srinivasan R <srinivasanr@gmail.com>
2011-10-14 22:11:38 +0530

    Converted to GET parameters instead of URL
    /expand?url=
    /e?url=
    /redirect?url=
    /r?url=

--- main.py
+++ main.py
@@ -24,15 +24,17 @@ class ExpandHandler(tornado.web.RequestHandler):
             if response.error:
                 print "Error:", response.error
             else:
+                print self.short_url
                 actual_url = response.effective_url
                 self.render("expanded.html", short_url=self.short_url, actual_url=actual_url)
         except:
             print traceback.format_exc()
     
     @tornado.web.asynchronous
-    def get(self, short_url):
-        self.short_url = short_url
-        get_head(short_url, self.handle_request)
+    def get(self):
+        print self.request.arguments
+        self.short_url = self.request.arguments['url'][0] # TODO: Maybe take in multiple URLs?
+        get_head(self.short_url, self.handle_request)
 
 
 class ExpandRedirectHandler(tornado.web.RequestHandler):
@@ -46,17 +48,19 @@ class ExpandRedirectHandler(tornado.web.RequestHandler):
             print traceback.format_exc()
 
     @tornado.web.asynchronous
-    def get(self, short_url):
+    def get(self):
         try:
+            short_url = self.request.arguments['url'][0]
             get_head(short_url, self.handle_redirect_request)
         except:
             print traceback.format_exc()
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/e/(.*)", ExpandHandler),
-    (r"/expand/(.*)", ExpandHandler),
-    (r"/(.*)", ExpandRedirectHandler),
+    (r"/e", ExpandHandler),
+    (r"/expand", ExpandHandler),
+    (r"/r", ExpandRedirectHandler),
+    (r"/redirect", ExpandRedirectHandler),
 ], debug=True, **settings)
 
 if __name__ == "__main__":
