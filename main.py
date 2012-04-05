e58c5d50b96f72d07d0a005ba3a16cae37339775
Srinivasan R <srinivasanr@gmail.com>
2012-04-05 17:15:16 -0300

    Removed ExpandRedirect Handler.
    Linking directly to the expanded URL now.
    And made the font size a bit small.

--- main.py
+++ main.py
@@ -57,36 +57,10 @@ class ExpandHandler(tornado.web.RequestHandler):
             self.render("expanded.html", short_url=short_url, actual_url=actual_url)
 
 
-class ExpandRedirectHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    @gen.engine
-    def get(self):
-        short_url = self.request.arguments['url'][0].strip().encode('ascii')
-        logging.info("Redirect: %s" % short_url)
-        url_md5 = md5.md5(short_url).hexdigest()
-        result = yield gen.Task(c.get, url_md5)
-        if result:
-            # Get URL from cache
-            actual_url = result
-            logging.info("From Cache")
-        else:
-            logging.info("From Request")
-            response = yield gen.Task(http_client.fetch, short_url, method='HEAD')
-            if response.error:
-                logging.error(response.error)
-                logging.error("Error for %s: %s" %(short_url, str(response.error)))
-                actual_url = '/'
-            else:
-                actual_url = response.effective_url
-                yield gen.Task(c.set, url_md5, actual_url)
-        self.redirect(actual_url, permanent=True)
-
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/e", ExpandHandler),
     (r"/expand", ExpandHandler),
-    (r"/r", ExpandRedirectHandler),
-    (r"/redirect", ExpandRedirectHandler),
 ], debug=True, **settings)
 
 if __name__ == "__main__":
